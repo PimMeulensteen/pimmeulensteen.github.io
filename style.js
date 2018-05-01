ab3024392b2f51a6184bdc4c1154420983528158
PimMeulensteen <pim@meulensteen.net>
2018-05-01 21:36:29 +0200

    Small bugfix for centering container

--- style.js
+++ style.js
@@ -1,2 +1,3 @@
 function name_div_width() { document.getElementById('container').style.width = (window.innerWidth > window.innerHeight ? window.innerHeight : "100%"); return 1 }
+name_div_width();
 window.onresize = name_div_width;
