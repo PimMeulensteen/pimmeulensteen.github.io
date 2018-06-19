aa8a867829b43565f70d1a9ab4c624b03ad751a6
Pim Meulensteen <PimMeulensteen@users.noreply.github.com>
2018-06-19 14:24:07 +0200

    JS kleiner gemaakt
    Javascript is nu een bestand, die aysnc geladen kan worden.

--- json.js
+++ json.js
@@ -1,4 +1,4 @@
-const items = '{"items":[ \
+const obj = JSON.parse('{"items":[ \
 	{"id":"dag",\
 	"title":"One-minute: Dag",\
 	"created":"8 februari 2018",\
@@ -53,5 +53,42 @@ const items = '{"items":[ \
 	"format":"png",\
 	"desc_short":"Met behulp van conditionele opmaak en een heel groot excel bestand kan je afbeeldingen weergeven in Excel."}\
 	]\
-}';
-load()
+}')
+
+var head = document.getElementsByTagName("main")[0];
+
+
+var l = obj.items.length;
+
+
+for (i = 0; i < l; i++) {
+	var id = obj.items[i].id;
+	var desc = obj.items[i].desc_short;
+
+	var article = document.createElement("article");
+
+	var im = document.createElement("img");
+	im.setAttribute("src", "/media/" + id + "." + obj.items[i].format);
+	im.setAttribute("alt", desc);
+	im.classList = "front";
+
+	var h2 = document.createElement("h2");
+	h2.innerHTML = obj.items[i].title;
+
+	var d = document.createElement("p");
+	d.innerHTML = obj.items[i].created;
+	d.classList = "date";
+
+	var p = document.createElement("p");
+	p.innerHTML = desc;
+
+	var b = document.createElement("section");
+	b.classList = "back"
+	b.appendChild(h2);
+	b.appendChild(d);
+	b.appendChild(p);
+	b.setAttribute("onclick", "location.href='/" + id + "/'");
+	article.appendChild(im);
+	article.appendChild(b);
+	head.appendChild(article);
+}
