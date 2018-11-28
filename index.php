736ab89939f6013be8668c9bdbdf10f4b5cba3d0
Pim Meulensteen <pimmeulensteen@gmail.com>
2018-11-28 21:07:29 +0100

    commit

--- index.php
+++ index.php
@@ -29,16 +29,15 @@
     h1,footer{margin:20px 0;text-align:center}
    
     main,header{display:flex;flex-wrap:wrap;align-content:stretch;flex-direction:row;justify-content:center;align-items:center}
-    article{margin:10px;width:280px;height:280px;position:relative;perspective:30rem;}
+    article{margin:10px;width:280px;height:280px;position:relative;perspective:30rem}
     p,h2{margin:20px}
 
-    .front,.back{position: absolute;backface-visibility:hidden;-webkit-backface-visibility:hidden;border-color:gray;border-width:1px;border-style:solid;transform-style:preserve-3d;}
+    .front,.back{position: absolute;backface-visibility:hidden;-webkit-backface-visibility:hidden;border-color:gray;border-width:1px;border-style:solid;transform-style:preserve-3d}
     .back{transform:rotateY(180deg);cursor:pointer}
     article:hover .front{transform:rotateY(180deg)}
     article:hover .back{transform:rotateY(360deg)}
     .seo-text{display: none;}
     @media only screen and (max-width: 619px) {article{width:80vw;height:80vw;margin:10vw 5vw}}
-
   </style>
 </head>
 
@@ -50,6 +49,12 @@
   </header>
   <main>
     <p class="seo-text">Website van Pim Meulensteen. Verzameling van mijn beste, leuks of interessanste projecten. Gemaakt voor lessen informatica in 5V in 2018.</p>
+    <?php
+    echo 'hello';
+    $string = file_get_contents("main.json");
+    $json_a = json_decode($string, true);
+    echo ($json_a);
+    ?>
   </main>
   <footer>
     Mail:&nbsp;<a href="mailto:pim@meulensteen.net">pim@meulensteen.net</a> |&nbsp;&copy;&nbsp;2018&nbsp;| Github:&nbsp;<a href="https://github.com/PimMeulensteen">/PimMeulensteen</a>
@@ -61,7 +66,7 @@
     //Doe dit dan keer elkaar. 
     let q = Math.min(4,Math.floor((window.innerWidth - 17)/300)) * (Math.ceil((window.innerHeight - 80)/300))
   </script>
-  <script src="json.js"></script>
+  <!-- <script src="json.js"></script> -->
 </body>
 
 
--- jd/demo/jd_up.js
+++ jd/demo/jd_up.js
@@ -1,113 +1,124 @@
-//Setup canvas in DOM
-var helperDiv = document.createElement("div");
-helperDiv.style.textAlign = "center";
-var canvas = document.createElement("canvas");
-var minSide = window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth; //Get smallest size.
-helperDiv.style.backgroundColor = 'black';
+const rand = function (min, max) {
+    return Math.random() * (max - min) + min
+}
+const randInt = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
 
-//Set style for the canvas
-canvas.style.height = window.innerHeight;
-canvas.style.width = minSide;
-canvas.style.margin = "0 auto";
-canvas.style.backgroundColor = 'black';
-canvas.style.marginBottom = '0';
-canvas.height = window.innerHeight;
-canvas.width = minSide;
+const normalRand = function (mu, sigma) {
+    //mu = mean
+    //sigma = standard deviation 
+    //normal distribution between -6 and 6
+    //This is not accurate, but that doesn't matter
+    let sum = 0
+    for (let i = 0; i < 6; i += 1) {
+        sum += rand(-1, 1)
+    }
+    let normal = sum / 6
+    return mu + sigma * normal
+}
 
-//Add elements to the DOM
-helperDiv.appendChild(canvas);
-document.body.appendChild(helperDiv);
+const normalPDF = function (x, mu, sigma) {
+    //See also https://en.wikipedia.org/wiki/Normal_distribution
+    //mu = mean
+    //sigma = standard devition
+    //sigma^2 = letiance.
+    let sigma2 = sigma ** 2
+    let p_1 = 1 / ((2 * Math.PI * sigma2) ** 0.5)
+    let p_2 = Math.exp(-((x - mu) ** 2) / (2 * sigma2))
+    return p_1 * p_2
+}
 
+const set_canvas_style = function (c) {
+    //Set style for the canvas 'c'
+    c.style.margin = "0 auto"
+    c.style.display = "block"
+    c.style.backgroundColor = 'black'
+    c.style.marginBottom = '0'
+}
+const set_body_style = function () {
+    body = document.getElementsByTagName('body')[0]
+    body.style.backgroundColor = 'black'
+}
 
-var smoothness = 50; //Higher = smoother animation
-var fps = 60;
+const set_canvas_size = function (c) {
+    minSide = window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth; //Get smallest size.
 
+    c.style.height = window.innerHeight
+    c.style.width = minSide
 
-//Setup for drawing of image
-var ctx = canvas.getContext('2d');
-ctx.lineWidth = 1.2;
-ctx.strokeStyle = 'white';
+    c.height = window.innerHeight
+    c.width = minSide
+}
+const set_contex = function (c) {
+    let ctx = c.getContext('2d')
+    ctx.lineWidth = 1.2
+    ctx.strokeStyle = 'white'
+    return ctx
+}
+
+let smoothness = 25; //Higher = smoother animation
+let fps = 30
+
+let canvas = document.createElement("canvas")
+
+set_canvas_style(canvas)
+set_body_style()
+
+set_canvas_size(canvas)
+document.body.appendChild(canvas);//Add canvas to the DOM
+ctx = set_contex(canvas)//Setup for drawing of image
+
 
 //100 px margin in each direction
-var xMin = minSide / 4;
-var xMax = minSide - xMin;
-var xMid = (xMin + xMax) / 2;
-var yMin = window.innerHeight/ 8;
-var yMax = window.innerHeight - yMin;
+let xMin = minSide / 5
+let xMax = minSide - xMin
+let xMid = (xMin + xMax) / 2
+let yMin = window.innerHeight / 12
+let yMax = window.innerHeight - yMin
 
 //n of lines and points
-var nLines = 80;
-var nPoints = 100;
+let nLines = 80
+let nPoints = 80
 
 
 
 //Delta's for points and line
-var dx = (xMax - xMin) / nPoints;
-var dy = (yMax - yMin) / nLines;
+let dx = (xMax - xMin) / nPoints
+let dy = (yMax - yMin) / nLines
 
 //Make array to store data of points
-var data = []
+let data = []
 for (i = 0; i < nLines; i++) {
     data.push(createLine())
 }
 
-function rand(min, max) {
-    return Math.random() * (max - min) + min
-}
-function randInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min
-}
-
-
-function normalRand(mu, sigma) {
-    //mu = mean
-    //sigma = standard deviation
-
-    //normal distribution between -6 and 6;
-    //Note: This is not accurate, but that doesn't matter; noise is welcome!
-    var sum = 0
-    for (var i = 0; i < 6; i += 1) {
-        sum += rand(-1, 1)
-    }
-    var normal = sum / 6
-    return mu + sigma * normal
-}
-
-function normalPDF(x, mu, sigma) {
-    //See also https://en.wikipedia.org/wiki/Normal_distribution
-    //mu = mean
-    //sigma = standard devition
-    //sigma^2 = variance.
-    var sigma2 = sigma ** 2;
-    var p_1 = 1 / ((2 * Math.PI * sigma2) ** 0.5);
-    var p_2 = Math.exp(-((x - mu) ** 2) / (2 * sigma2));
-    return p_1 * p_2;
-}
 
 function createLine() {
-    x = xMin;
-    var line = []
+    x = xMin
+    let line = []
 
-    var nModes = randInt(1, 4) //Number of "peaks" in a line
+    let nModes = randInt(1, 4) //Number of "peaks" in a line
 
-    var mus = []
-    var sigmas = []
-    for (var j = 0; j < nModes; j++) {
+    let mus = []
+    let sigmas = []
+    for (let j = 0; j < nModes; j++) {
         mus[j] = normalRand(xMid, 100)
         sigmas[j] = normalRand(24, 30)
     }
 
-    for (var j = 0; j < nPoints; j++) {
-        x = x + dx;
+    for (let j = 0; j < nPoints; j++) {
+        x = x + dx
         y = 0
-        for (var l = 0; l < nModes; l++) {
+        for (let l = 0; l < nModes; l++) {
             y += normalPDF(x, mus[l], sigmas[l])
         }
         y = (y * 1000) + (2 * Math.random())
         line.push([x, y])
     }
-    return line;
+    return line
 }
+
 function shift() {
     data.shift()
     data.push(createLine())
@@ -115,33 +126,39 @@ function shift() {
 
 function drawImage() {
     //Shift the array every "smoothness" frames, and reset framecounter
-    frames = (frames + 1) % smoothness;
+    frames = (frames + 1) % smoothness
     if (frames == 0) {
-        shift();
+        shift()
     }
-    //Make canvas black to redraw
-    ctx.fillStyle = 'black'
+
     ctx.fillRect(0, 0, canvas.width, canvas.height)
     ctx.strokeStyle = '#A0A0A0'; //Set draw color to gray 
 
-    for (var i = 0; i < nLines; i++) {
+    for (let i = 0; i < nLines; i++) {
+        //Start drwaing the path
         ctx.beginPath()
-        for (var j = 0; j < nPoints; j++) {
+        for (let j = 0; j < nPoints; j++) {
             //First and last line don't move, while everything inbetween does.
             if (i == nLines - 1) {
-                y = -data[i][j][1] + dy * i + yMin - dy;
+                y = -data[i][j][1] + dy * i + yMin - dy
             } else if (i == 0) {
-                y = -data[i][j][1] + dy * i + yMin;
+                y = -data[i][j][1] + dy * i + yMin
             } else {
                 y = -data[i][j][1] + dy * i + yMin - (frames / smoothness * dy)
             }
             //Load data from array and draw line
-            ctx.lineTo(data[i][j][0], y);
+            ctx.lineTo(data[i][j][0], y)
         }
+        //Finish drwaing the path
         ctx.fill()
-        ctx.stroke();
+        ctx.stroke()
     }
 }
 
-var frames = 0;
-setInterval(drawImage, 1000 / fps)
+let frames = 0
+let interval = setInterval(drawImage, 1000 / fps)
+window.addEventListener('resize', function () {
+    this.clearInterval(interval)
+    set_canvas_size(canvas)
+    interval = setInterval(drawImage, 1000 / fps)
+})
